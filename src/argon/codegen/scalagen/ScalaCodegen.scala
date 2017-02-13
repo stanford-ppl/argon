38791143f1f1fbe56694e6834f0a3d9072d55bfb
mattfel1 <mattfel@stanford.edu>
2017-02-12 17:14:24 -0800

    fix scala depencies

--- src/argon/codegen/scalagen/ScalaCodegen.scala
+++ src/argon/codegen/scalagen/ScalaCodegen.scala
@@ -1,8 +1,12 @@
 package argon.codegen.scalagen
 
 import argon.codegen.Codegen
+import sys.process._
+import scala.language.postfixOps
+import argon.codegen.FileDependencies
 
-trait ScalaCodegen extends Codegen {
+trait ScalaCodegen extends Codegen with FileDependencies{
+	
   import IR._
   override val name = "Scala Codegen"
   override val lang: String = "scala"
@@ -15,7 +19,7 @@ trait ScalaCodegen extends Codegen {
 
   override def copyDependencies(out: String): Unit = {
     // FIXME: Should be OS-independent. Ideally want something that also supports wildcards, maybe recursive copy
-    s"""cp -r ${sys.env("SPATIAL_HOME")}/src/spatial/codegen/scalagen/resources/Makefile ${out}""".!
+    s"""cp ${sys.env("SPATIAL_HOME")}/src/spatial/codegen/scalagen/resources/Makefile ${out}/..""".!
     s"""cp ${sys.env("SPATIAL_HOME")}/src/spatial/codegen/chiselgen/resources/run.sh ${out}/..""".!
     super.copyDependencies(out)
   }
