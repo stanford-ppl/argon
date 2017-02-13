5349684b5ec961f496ef3dc37023c7f443d64cca
mattfel1 <mattfel@stanford.edu>
2017-02-13 00:00:31 -0800

    fix block reduce in cpp

--- src/argon/codegen/cppgen/CppGenIfThenElse.scala
+++ src/argon/codegen/cppgen/CppGenIfThenElse.scala
@@ -1,14 +1,24 @@
 package argon.codegen.cppgen
 
 import argon.ops.IfThenElseExp
+import argon.ops.{ArrayExtExp, TextExp, FixPtExp, FltPtExp, BoolExp}
 
-trait CppGenIfThenElse extends CppCodegen {
-  val IR: IfThenElseExp
+
+trait CppGenIfThenElse extends CppGenArray {
+  val IR: IfThenElseExp with ArrayExtExp with TextExp with FixPtExp with FltPtExp with BoolExp
   import IR._
+
+
 
   override protected def emitNode(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
     case IfThenElse(cond, thenp, elsep) =>
-      emit(src"${lhs.tp} $lhs;")
+      // Initialization of lhs is funky if we have array type
+      if (isArrayType(lhs.tp)) {
+        emit(src"// TODO: IfThenElse to set array type is tricky, only works for 1D now")
+        emit(src"${lhs.tp}* $lhs;")
+      } else { 
+        emit(src"${lhs.tp} $lhs;")
+      }
       open(src"if ($cond) { ")
       emitBlock(thenp)
       emit(src"$lhs = ${thenp.result};")
@@ -16,6 +26,7 @@ trait CppGenIfThenElse extends CppCodegen {
       emitBlock(elsep)
       emit(src"$lhs = ${elsep.result};")
       close("}")
+
 
     case _ => super.emitNode(lhs, rhs)
   }
