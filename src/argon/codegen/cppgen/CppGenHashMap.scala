30b992a6f7ead53f1f5ae1093eeef7bc39ba00d9
mattfel1 <mattfel@stanford.edu>
2017-02-27 20:07:07 -0800

    fix hashmap segfaults

--- src/argon/codegen/cppgen/CppGenHashMap.scala
+++ src/argon/codegen/cppgen/CppGenHashMap.scala
@@ -15,9 +15,10 @@ trait CppGenHashMap extends CppCodegen {
   override protected def emitNode(lhs: Sym[_], rhs: Op[_]) = rhs match {
     case HashIndexApply(index, key) => 
       emit(src"${lhs.tp} $lhs = -1;")
-      open(src"for (int ${lhs}_i; ${lhs}_i < ${index}->length; ${lhs}_i++) {")
+      open(src"for (int ${lhs}_i = 0; ${lhs}_i < ${index}_size; ${lhs}_i++) {")
       emit(src"if (${index}->apply(${lhs}_i) == ${key}) { $lhs = ${lhs}_i; }")
       close("}")
+      emit(src"assert(${lhs} > -1);")
     case _ => super.emitNode(lhs, rhs)
   }
 
@@ -26,7 +27,7 @@ trait CppGenHashMap extends CppCodegen {
       emit(src"cppDeliteArray${e.mK}* ${quote(lhs(0))} = new cppDeliteArray${e.mK}(${in}->length); // Keys")
       emit(src"cppDeliteArray${e.mV}* ${lhs(1)} = new cppDeliteArray${e.mV}(${in}->length); // Values")
       emit(src"cppDeliteArray${e.mK}* ${lhs(2)} = ${lhs(0)}; // TODO: Probably totally wrong.  lhs2 appears to be the scala hashmap, but it seems like we only use it to lookup index of a key")
-      emit(s"long size = 0;")
+      emit(src"long ${lhs(2)}_size = 0;")
       //emit(src"//val ${lhs(2)}  = new ${HashIndexType(e.mK)}()")
       open(src"for (int $i = 0; $i < ${in}->length; $i++) { ")
         emit(s"// Apply block")
@@ -37,17 +38,17 @@ trait CppGenHashMap extends CppCodegen {
         visitBlock(valFunc)
         emit(s"bool contained = false;")
         emit(s"long idx = -1;")
-        emit(src"for (int i = 0; i < size; i++) {if (${lhs(0)}->apply(i) == ${keyFunc.result}) {contained = true;}}")
+        emit(src"for (int i = 0; i < ${lhs(2)}_size; i++) {if (${lhs(0)}->apply(i) == ${keyFunc.result}) {contained = true; idx = i;}}")
         open(src"if (contained) {")
           emit(src"${rV._1.tp}* ${rV._1} = ${valFunc.result};")
           emit(src"${rV._2.tp}* ${rV._2} = ${lhs(1)}->apply(idx);")
           visitBlock(reduce)
           emit(src"${lhs(1)}->update(idx, ${reduce.result});")
         closeopen("} else {")
-          emit(s"size += 1;")
-          emit(src"//index += ${keyFunc.result} -> ${lhs(1)}.size")
-          emit(src"${lhs(0)}->update(size, ${keyFunc.result});")
-          emit(src"${lhs(1)}->update(size, ${valFunc.result});")
+          emit(src"//index += ${keyFunc.result} -> ${lhs(1)}.${lhs(2)}_size")
+          emit(src"${lhs(0)}->update(${lhs(2)}_size, ${keyFunc.result});")
+          emit(src"${lhs(1)}->update(${lhs(2)}_size, ${valFunc.result});")
+          emit(src"${lhs(2)}_size += 1;")
         close("}")
       close("}")
 
