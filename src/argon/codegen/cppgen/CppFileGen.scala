3d5d43b18989201afe6a669567a4085141ef2820
mattfel1 <mattfel@stanford.edu>
2017-03-07 10:37:35 -0800

    cpp changes

--- src/argon/codegen/cppgen/CppFileGen.scala
+++ src/argon/codegen/cppgen/CppFileGen.scala
@@ -26,8 +26,7 @@ trait CppFileGen extends FileGen {
 
 
   override protected def emitFileHeader() {
-    emit(s"""#include "interface.h"
-#include <stdint.h>
+    emit(s"""#include <stdint.h>
 #include <sys/time.h>
 #include <iostream>
 #include <fstream>
@@ -47,72 +46,12 @@ trait CppFileGen extends FileGen {
 #include "cppDeliteArraydouble.h"
 #include "FringeContext.h"
 
-void Top_run( Interface_t *args, int n_args, ... )
-{
-  // Constant fringe stuff
-
-  // Create an execution context.
-  FringeContext *c1 = new FringeContext();
-
-  // Program FPGA
-  c1->load();
-
-  // Set input arguments
-  int numArgIns = sizeof(args->ArgIns)/sizeof(args->ArgIns[0]);
-  for (int i=0; i<numArgIns; i++) {
-    c1->setArg(i, (uint64_t)args->ArgIns[i]);
-  }
-
-  va_list ap;
-  va_start(ap, n_args);
-  for(int i = 1; i <= n_args; i++) {
-    cppDeliteArrayint32_t* array = va_arg(ap, cppDeliteArrayint32_t*);
-    uint64_t addr = c1->malloc(2 * array->length * sizeof(array->apply(0)));
-    c1->setArg(numArgIns+i-1, addr);
-    std::cout << "Setting memStream " << i << " to " << addr << std::endl;
-    c1->memcpy(addr, array, 2 * array->length * sizeof(array->apply(0))); // No idea why we need to double the size, or else last 8 words get chopped...
-  }
-  va_end(ap);
-
-  // Run FPGA
-  c1->run();
-
-  // Read output arguments
-  int numArgOuts = sizeof(args->ArgOuts)/sizeof(args->ArgOuts[0]);
-  for (int i = 0; i < numArgOuts; i++) {
-    *args->ArgOuts[i] = c1->getArg(i);
-  }
-
-}
 """)
 
   open(s"void Application(int numThreads, cppDeliteArraystring * args) {")
-  emit("Interface_t interface;")
+  emit("// Create an execution context.")
+  emit("FringeContext *c1 = new FringeContext();")
 
-    withStream(getStream("interface", "h")) {
-      emit(s"""// Interface between delite c++ and hardware tester
-  // class Interface_t {
-
-  // public:
-  //   int32_t* ArgIns[1]; // Can do sizeof this because we size arrays at codegen time
-  //   int32_t* ArgOuts[1];
-  //   long* MemIns[0];
-  //   long* MemOuts[0][0];
-  //   uint64_t* cycles;
-  // };
-  
-#include <vector>
-#include <iostream>
-#include <stdint.h>
-
-class Interface_t""")
-      open("{")
-      open(s"""public:""")
-      emit("Interface_t()")
-      emit("{}")
-      emit("~Interface_t()")
-      emit("{}")
-    }
 
     withStream(getStream("cpptypes","h")) {
       emit("""#ifndef __CPPTYPES_H__
@@ -168,16 +107,6 @@ int main(int argc, char *argv[]) {
 }
 """)
 
-    withStream(getStream("interface", "h")) {
-      emit(s"uint64_t* cycles;")
-      emit(s"void add_mem(long num) { memOut.push_back(num);}")
-      emit(s"long get_mem(int i) { return memOut[i]; }")
-      emit(s"long memOut_length() { return memOut.size(); } // Cannot size in advance because multiple DRAMs will share this header")
-      emit(s"private:")
-      emit(s"std::vector<long> memOut;")
-      close("")
-      close("};")
-    }
 
     super.emitFileFooter()
   }
