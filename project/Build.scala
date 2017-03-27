0ccc097b52584a07841c18b2b6ddcfd8adcb6ce3
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-03-27 19:10:32 +0200

    scalameta build file

--- project/Build.scala
+++ project/Build.scala
@@ -1,25 +1,26 @@
 import sbt._
-import Keys._
+import Keys.{resolvers, _}
 
 object ArgonBuild extends Build {
   //lazy val ARGON_HOME = sys.env.get("ARGON_HOME").getOrElse(error("Please set the ARGON_HOME environment variable."))
     
   val compilerVersion = "2.12.1"
   val scalatestVersion = "3.0.1"
-  val paradiseVersion = "2.1.0"  // check here: https://github.com/scalamacros/paradise/releases
-  //val paradiseVersion = "3.0.0-M7"
+  val paradiseVersion = "3.0.0-M7"
+  val metaVersion = "1.6.0"
 
-  lazy val virtBuildSettings = Defaults.defaultSettings ++ Seq(
-
-    organization := "stanford-ppl",
+  lazy val virtBuildSettings = Defaults.coreDefaultSettings ++ Seq(
+    organization := "org.virtualized",
     scalaVersion := compilerVersion,
 
+    resolvers += Resolver.sonatypeRepo("releases"),
+    resolvers += Resolver.bintrayIvyRepo("scalameta", "maven"),
+
     publishArtifact in (Compile, packageDoc) := false,
-//    libraryDependencies += "org.virtualized" %% "virtualized" % "0.1",
-//    libraryDependencies += "org.virtualized" %% "virtualized" % "0.9-SNAPSHOT",
     libraryDependencies += "org.scalatest" %% "scalatest" % scalatestVersion % "test",
     libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value % "compile",
-    libraryDependencies += "org.apache.commons" % "commons-lang3" % "3.3.2",
+    libraryDependencies += "org.scalameta" %% "scalameta" % metaVersion,
+    libraryDependencies += "org.scalameta" %% "contrib" % metaVersion,
 
     retrieveManaged := true,
     scalacOptions += "-Yno-generic-signatures",
@@ -32,7 +33,7 @@ object ArgonBuild extends Build {
     scalacOptions ++= Seq("-language:higherKinds", "-language:implicitConversions"),
 
     scalacOptions in (Compile, doc) ++= Seq(
-      "-doc-root-content", 
+      "-doc-root-content",
       baseDirectory.value+"/root-doc.txt",
       "-diagrams",
       "-diagrams-debug",
@@ -40,20 +41,26 @@ object ArgonBuild extends Build {
       "-doc-title", name.value
     ),
 
-    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full),
-  
-//    addCompilerPlugin("org.scalameta" % "paradise" % paradiseVersion cross CrossVersion.full),
+    addCompilerPlugin("org.scalameta" % "paradise" % paradiseVersion cross CrossVersion.full),
 
-    scalaSource in Compile <<= baseDirectory(_ / "src"),
-    scalaSource in Test <<= baseDirectory(_ / "test"),
+    scalacOptions += "-Xplugin-require:macroparadise",
+    // temporary workaround for https://github.com/scalameta/paradise/issues/10
+    scalacOptions in (Compile, console) := Seq(), // macroparadise plugin doesn't work in repl yet.
+    // temporary workaround for https://github.com/scalameta/paradise/issues/55
+    sources in (Compile, doc) := Nil, // macroparadise doesn't work with scaladoc yet.
+
+    scalaSource in Compile := baseDirectory(_/ "src").value,
+    scalaSource in Test := baseDirectory(_/"test").value,
 
     parallelExecution in Test := false,
-    concurrentRestrictions in Global += Tags.limitAll(1) //we need tests to run in isolation across all projects
+    concurrentRestrictions in Global += Tags.limitAll(1) // we need tests to run in isolation across all projects
   )
+
   lazy val argonSettings = virtBuildSettings ++ Seq(
     name := "argon",
     version := "1.0",
-    isSnapshot := true 
+    isSnapshot := true,
+    libraryDependencies += "org.apache.commons" % "commons-lang3" % "3.3.2"
   )
 
 
