beeb7b1ed0a7e1e2b73aae8d96e3fbc6a59a53e1
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-04-17 01:12:56 -0700

    function generate working

--- core/src/argon/ops/Functions.scala
+++ core/src/argon/ops/Functions.scala
@@ -16,19 +16,19 @@ trait FunctionApi extends FunctionExp {
 trait FunctionExp {
   self: ArgonExp =>
 
-  case class FunApplyJJ$JJ$1to22[TII$II$1toJJ:Type, R:Type](fun: Exp[ArgonFunctionJJ[TII$II$1toJJ, R]], argII$II$1toJJ: Exp[TII]) extends Op[R] {
-    def mirror(f: Tx): Exp[R] = fun_applyJJ(f(fun), f$II$1toJJ(argII))
+  case class FunApplyJJ$JJ$1to22[TII$II$1toJJ, R:Type](fun: Exp[ArgonFunctionJJ[TII$II$1toJJ, R]], argII$II$1toJJ: Exp[TII])(implicit evII$II$1toJJ: Type[TII]) extends Op[R] {
+    def mirror(f: Tx): Exp[R] = fun_applyJJ(f(fun), argII$II$1toJJ)
   }
 
-  case class FunDeclJJ$JJ$1to22[TII$II$1toJJ:Type, R:Type](argII$II$1toJJ: Exp[TII], block: Block[R]) extends Op[ArgonFunctionJJ[TII$II$1toJJ,R]] {
-    def mirror(f: Tx) = stage(FunDeclJJ(f$II$1toJJ(argII), stageBlock { f(block) }))(ctx)
+  case class FunDeclJJ$JJ$1to22[TII$II$1toJJ, R:Type](argII$II$1toJJ: Exp[TII], block: Block[R])(implicit evII$II$1toJJ: Type[TII]) extends Op[ArgonFunctionJJ[TII$II$1toJJ,R]] {
+    def mirror(f: Tx) = stage(FunDeclJJ(argII$II$1toJJ, stageBlock { f(block) }))(ctx)
     override def binds = dyns(argII$II$1toJJ) ++ super.binds
   }
 
 
-  def fun_applyJJ$JJ$1to22[TII$II$1toJJ:Type, R:Type](f: Exp[ArgonFunctionJJ[TII$II$1toJJ,R]], arg: Exp[T1])(implicit ctx: SrcCtx): Exp[R] = stage(FunApplyJJ(f, arg))(ctx)
+  def fun_applyJJ$JJ$1to22[TII$II$1toJJ, R:Type](f: Exp[ArgonFunctionJJ[TII$II$1toJJ,R]], argII$II$1toJJ: Exp[TII])(implicit evII$II$1toJJ: Type[TII], ctx: SrcCtx): Exp[R] = stage(FunApplyJJ(f, argII$II$1toJJ))(ctx)
 
-  case class ArgonFunctionJJ$JJ$1to22[TII$II$1toJJ:Type, R:Type](s: Exp[ArgonFunctionJJ[TII$II$1toJJ, R]]) extends MetaAny[ArgonFunctionJJ[TII$II$1toJJ,R]] with FunctionJJ[TII$II$1toJJ, R] {
+  case class ArgonFunctionJJ$JJ$1to22[TII$II$1toJJ, R:Type](s: Exp[ArgonFunctionJJ[TII$II$1toJJ, R]])(implicit evII$II$1toJJ: Type[TII]) extends MetaAny[ArgonFunctionJJ[TII$II$1toJJ,R]] with FunctionJJ[TII$II$1toJJ, R] {
     @api def ===(that: ArgonFunctionJJ[TII$II$1toJJ, R]) = ???
     @api def =!=(that: ArgonFunctionJJ[TII$II$1toJJ, R]) = ???
     @api def toText = textify(this)
@@ -37,23 +37,26 @@ trait FunctionExp {
     @api def applyArg(xII$II$1toJJ: TII): R = wrap(fun_applyJJ(s, xII$II$1toJJ.s))
   }
 
-  def fun$JJ$1to22[TII$II$1toJJ:Type, R:Type](f: FunctionJJ[TII$II$1toJJ, R])(implicit ctx: SrcCtx): ArgonFunction1[TII$II$1toJJ,R] = {
+  def fun$JJ$1to22[TII$II$1toJJ, R:Type](f: FunctionJJ[TII$II$1toJJ, R])(implicit evII$II$1toJJ: Type[TII], ctx: SrcCtx): ArgonFunctionJJ[TII$II$1toJJ,R] = {
     val argII$II$1toJJ = fresh[TII]
-    val bodyBlock = stageBlock(f(wrap$II$1toJJ(argII)).s)
+    val wargII$II$1toJJ = wrap(argII)
+    val bodyBlock = stageBlock(f(wargII$II$1toJJ).s)
     val sym = stage(FunDeclJJ(argII$II$1toJJ, bodyBlock))(ctx)
     wrap(sym)
   }
 
   /** Type classes **/
-  case class ArgonFunctionJJType$JJ$1to22[TII$II$1toJJ, R](childTII$II$1toJJ: Meta[TII], childR: Meta[R]) extends Meta[ArgonFunctionJJ[TII$II$1toJJ, R]] {
-    override def wrapped(x: Exp[ArgonFunctionJJ[TII$II$1toJJ,R]]) = ArgonFunctionJJ(x)(childTII$II$1toJJ, childR)
+  case class ArgonFunctionJJType$JJ$1to22[TII$II$1toJJ, R](childTII$II$1toJJ: Type[TII], childR: Type[R]) extends Meta[ArgonFunctionJJ[TII$II$1toJJ, R]] {
+    override def wrapped(x: Exp[ArgonFunctionJJ[TII$II$1toJJ,R]]) = ArgonFunctionJJ(x)(childR, childTII$II$1toJJ)
     override def unwrapped(x: ArgonFunctionJJ[TII$II$1toJJ,R]) = x.s
     override def stagedClass = classOf[ArgonFunctionJJ[TII$II$1toJJ,R]]
     override def typeArguments = List(childTII$II$1toJJ, childR)
     override def isPrimitive: Boolean = false
   }
 
-  implicit def argonFunctionJJ$JJ$1to22[TII$II$1toJJ:Meta, R:Meta]: ArgonFunctionJJType[TII$II$1toJJ,R] = ArgonFunctionJJType[TII$II$1toJJ,R](meta$II$1toJJ[TII], meta[R])
+  implicit def argonFunctionJJ$JJ$1to22[TII$II$1toJJ, R:Type](implicit evII$II$1toJJ: Type[TII]): ArgonFunctionJJType[TII$II$1toJJ,R] = {
+    ArgonFunctionJJType(evII$II$1toJJ, meta[R])
+  }
 
 
 
