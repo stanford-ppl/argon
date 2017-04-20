d33e7203f4921d8e99e1410a8722c1a5c0046d48
Matthew Feldman <mattfel@stanford.edu>
2017-04-20 11:38:09 -0700

    implement flt ops

--- core/src/argon/codegen/cppgen/CppGenFltPt.scala
+++ core/src/argon/codegen/cppgen/CppGenFltPt.scala
@@ -14,32 +14,32 @@ trait CppGenFltPt extends CppCodegen {
   }
 
   override protected def quoteConst(c: Const[_]): String = (c.tp, c) match {
-    case (FloatType(), Const(c: BigDecimal)) => c.toString + "f"
+    case (FloatType(), Const(c: BigDecimal)) => c.toString
     case (DoubleType(), Const(c: BigDecimal)) => c.toString
     case _ => super.quoteConst(c)
   }
 
   override protected def emitNode(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
-    case FltNeg(x)   => emit(src"val $lhs = -$x")
-    case FltAdd(x,y) => emit(src"val $lhs = $x + $y")
-    case FltSub(x,y) => emit(src"val $lhs = $x - $y")
-    case FltMul(x,y) => emit(src"val $lhs = $x * $y")
-    case FltDiv(x,y) => emit(src"val $lhs = $x / $y")
-    case FltLt(x,y)  => emit(src"val $lhs = $x < $y")
-    case FltLeq(x,y) => emit(src"val $lhs = $x <= $y")
-    case FltNeq(x,y) => emit(src"val $lhs = $x != $y")
-    case FltEql(x,y) => emit(src"val $lhs = $x == $y")
+    case FltNeg(x)   => emit(src"${lhs.tp} $lhs = -$x;")
+    case FltAdd(x,y) => emit(src"${lhs.tp} $lhs = $x + $y;")
+    case FltSub(x,y) => emit(src"${lhs.tp} $lhs = $x - $y;")
+    case FltMul(x,y) => emit(src"${lhs.tp} $lhs = $x * $y;")
+    case FltDiv(x,y) => emit(src"${lhs.tp} $lhs = $x / $y;")
+    case FltLt(x,y)  => emit(src"${lhs.tp} $lhs = $x < $y;")
+    case FltLeq(x,y) => emit(src"${lhs.tp} $lhs = $x <= $y;")
+    case FltNeq(x,y) => emit(src"${lhs.tp} $lhs = $x != $y;")
+    case FltEql(x,y) => emit(src"${lhs.tp} $lhs = $x == $y;")
     case FltRandom(x) => lhs.tp match {
-      case FloatType()  => emit(src"val $lhs = cpp.util.Random.nextFloat()")
-      case DoubleType() => emit(src"val $lhs = cpp.util.Random.nextDouble()")
+      case FloatType()  => emit(src"${lhs.tp} $lhs = cpp.util.Random.nextFloat();")
+      case DoubleType() => emit(src"${lhs.tp} $lhs = cpp.util.Random.nextDouble();")
     }
     case FltConvert(x) => lhs.tp match {
-      case FloatType()  => emit(src"val $lhs = $x.toFloat")
-      case DoubleType() => emit(src"val $lhs = $x.toDouble")
+      case FloatType()  => emit(src"${lhs.tp} $lhs = (${lhs.tp}) $x;")
+      case DoubleType() => emit(src"${lhs.tp} $lhs = (${lhs.tp}) $x;")
     }
     case FltPtToFixPt(x) => lhs.tp match {
-      case IntType()  => emit(src"${lhs.tp} $lhs = $x;")
-      case LongType() => emit(src"${lhs.tp} $lhs = $x;")
+      case IntType()  => emit(src"${lhs.tp} $lhs = (${lhs.tp}) $x;")
+      case LongType() => emit(src"${lhs.tp} $lhs = (${lhs.tp}) $x;")
       case FixPtType(s,d,f) => emit(src"${lhs.tp} $lhs = $x;")
     }
     case StringToFltPt(x) => lhs.tp match {
