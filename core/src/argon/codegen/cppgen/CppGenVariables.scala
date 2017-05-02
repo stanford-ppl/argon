05a21c14d063aced1769d13de17e07f929bae008
Matthew Feldman <mattfel@stanford.edu>
2017-05-02 14:00:20 -0700

    bug with var type

--- core/src/argon/codegen/cppgen/CppGenVariables.scala
+++ core/src/argon/codegen/cppgen/CppGenVariables.scala
@@ -9,7 +9,7 @@ trait CppGenVariables extends CppCodegen {
 
 
   override protected def emitNode(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
-    case NewVar(init)    => emit(src"${lhs.tp} $lhs = $init;")
+    case NewVar(init)    => emit(src"${lhs.tp.typeArguments.head} $lhs = $init;")
     case ReadVar(v)      => emit(src"${lhs.tp} $lhs = $v;")
     case AssignVar(v, x) => emit(src"$v = $x;")
     case _ => super.emitNode(lhs, rhs)
