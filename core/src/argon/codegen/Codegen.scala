ce5dda49ec05e379f1a5ee7c751936c89482c155
Matthew Feldman <mattfel@stanford.edu>
2017-07-28 19:33:17 -0700

    quote for bigint

--- core/src/argon/codegen/Codegen.scala
+++ core/src/argon/codegen/Codegen.scala
@@ -148,6 +148,7 @@ trait Codegen extends Traversal {
     case b: Boolean => b.toString
     case l: Long => l.toString
     case l: BigDecimal => l.toString
+    case l: BigInt => l.toString
     case _ => throw new RuntimeException(s"Could not quote or remap $arg (${arg.getClass})")
   }
 
